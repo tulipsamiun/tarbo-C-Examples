9577586e4aec5b8535f6388b1ccef75ade869ae5
tulipsamiun <tsamiun_ts@yahoo.com>
2021-12-04 03:17:28 +0600

    Create Factorial.c

---
+++ Factorial.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int fact1(int n){
+    if ( n == 0)
+        return 1;
+    else if ( n < 0)
+        return -1;
+    else 
+        return (n * fact1(n-1));
+}
+
+void main()
+{
+    int n, fact;
+    printf("Enter Value For n = ");
+    scanf("%d", &n);
+    fact = fact1(n);
+    if(fact == -1 )
+         printf("Factorial of a negative number does not exists");
+    else
+    printf("Factorial = %d", fact);
+    getch();
+}
