51cf551c90e429203fdf9a977aa0930db441af1e
tulipsamiun <tsamiun_ts@yahoo.com>
2021-12-06 05:22:26 +0600

    Create BinaryToDecimal.c

---
+++ BinaryToDecimal.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+#include <string.h>
+
+void main()
+{
+    int i, n, dec = 0;
+    char bin[10];
+    clrscr();
+    printf("BinaryToDecimal.cppEnter Decimal Number = ");
+    gets(bin);
+    n = strlen(bin);
+    n = n - 1;
+    for( i = n; i >= 0; i--){
+        dec = dec + ((bin[i] - 48) * pow(2,n-i));
+    }
+    printf("%d", dec);
+    getch();
+}
