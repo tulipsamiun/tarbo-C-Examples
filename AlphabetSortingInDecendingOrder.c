3bc565d0591a7f4367c416f6a514e0dbec3e51b4
tulipsamiun <tsamiun_ts@yahoo.com>
2022-01-07 05:14:04 +0600

    Create AlphabetSortingInDecendingOrder.c

---
+++ AlphabetSortingInDecendingOrder.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <conio.h>
+#include <ctype.h>
+
+
+void main()
+{
+    int i, j, n;
+    char element[20], temp;
+    printf("How Many Character You Want To Enter = ");
+    scanf("%d", &n);
+    printf("Enter Elements = ");
+    for(i = 0; i < n ; i ++){
+        scanf("%s", &element[i]);
+    }
+    for(i =0; i < n ;i++){    
+        for(j= i+1; j < n; j++){
+            if(toupper(element[i]) < toupper(element[j])){
+                temp = element[j];
+                element[j] = element[i];
+                element[i] =  temp;
+            }
+        }
+    }
+    printf("Rechorded List of Character = ");
+    for(i =0 ;i < n; i++){
+        printf("\n%c", element[i]);
+    }
+    getch();
+}
