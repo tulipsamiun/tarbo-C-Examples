b8e001b3deb292ddde818f1cd47d2d1e32e38d38
tulipsamiun <tsamiun_ts@yahoo.com>
2022-06-13 06:37:48 +0600

    Create ReverseUpperAndLowerCaseCharrecter.c

---
+++ ReverseUpperAndLowerCaseCharrecter.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+#include <ctype.h>
+
+void main()
+{
+    int count, tag;
+    char c, text[20];
+    printf("Hello World");
+    printf("%s","Enter a Line Of Text Bellow");
+    for(count = 0; (text[count] = getchar()) != '\n'; ++count)
+        tag = count;
+    printf("%d", tag);
+    for(count = 0; count < tag; ++count) {
+        c = islower(text[count]) ? toupper(text[count]):tolower(text[count]);
+    
+    putchar(c);
+    }
+    getch();
+}
+
